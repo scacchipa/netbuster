40ad7f37a5d65788154ac3fc3aa4baf4282fe988
Pablo Antonio Scacchi Bernasconi <scacchipa@gmail.com>
2021-07-17 02:07:33 -0300

    Initial commit with 2 initial test

---
+++ app/src/androidTest/java/ar/com/westsoft/netbuster/ExampleInstrumentedTest.kt
@@ -0,0 +1,58 @@
+package ar.com.westsoft.netbuster
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.volley.toolbox.StringRequest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    lateinit var appContext: Context
+    lateinit var tvAPIClient: TvAPIClient
+    @Before
+    fun setup() {
+        appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        tvAPIClient = TvAPIClient(appContext)
+    }
+    @Test
+    fun useAppContext() {
+        assertEquals("ar.com.westsoft.netbuster", appContext.packageName)
+    }
+    @Test
+    fun singleRequest() {
+        // Context of the app under test.
+
+        val signal = CountDownLatch(1)
+        val url = "https://api.tvmaze.com/"
+        var stringResponse = ""
+        val stringRequest = StringRequest(
+            "$url/search/shows?q=:query",
+            { response ->
+                stringResponse = response
+                signal.countDown()
+            },
+            { error ->
+                error.printStackTrace()
+                signal.countDown()
+            })
+
+        tvAPIClient.queue.add(stringRequest)
+        signal.await()
+        assertEquals(stringResponse.length, 3381)
+    }
+    @Test
+    fun asynGetResponse() = runBlocking {
+        assertEquals(tvAPIClient.getSyncResponse().length, 3381)
+    }
+}
---
+++ app/src/main/java/ar/com/westsoft/netbuster/TvAPIClient.kt
@@ -0,0 +1,38 @@
+package ar.com.westsoft.netbuster
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class TvAPIClient(context: Context) {
+    val url: String = "https://api.tvmaze.com/"
+    var queue: RequestQueue = Volley.newRequestQueue(context)
+
+    fun getRequest() {
+
+        val stringRequest = StringRequest(
+            "$url/search/shows?q=:query",
+            { response ->
+                Log.i("Response", "Response is: ${response.substring(1..100)}")
+            },
+            { error ->
+                error.printStackTrace()
+            })
+        queue.add(stringRequest)
+    }
+
+    suspend fun getSyncResponse(): String =
+        suspendCoroutine { continuation ->
+                val stringRequest = StringRequest(
+                    "$url/search/shows?q=:query",
+                    { response -> continuation.resume(response) },
+                    { continuation.resumeWithException(it) })
+                queue.add(stringRequest)
+        }
+
+}
